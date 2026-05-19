1bbb4025b87228306ced1756b556aa88121437e4
Dilyaraaa19 <dilyarka00@mail.ru>
2026-05-19 17:09:59 +0300

    Delete IdeaProjects/Project final directory

--- IdeaProjects/Project final/src/dfdsn.java
+++
@@ -1,8 +0,0 @@
-public class dfdsn {
-        public static void main(String[] args) {
-            System.out.println("рвислрл");
-            System.out.println("764782634");
-            System.out.println("bgkjbkjdsf");
-        }
-}
-
