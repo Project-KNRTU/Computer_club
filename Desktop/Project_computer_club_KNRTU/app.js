6245509d480bfd8ff24f93e8e931a7acef659f00
Dilyara <dilyarka00@mail.ru>
2026-05-24 00:14:58 +0300

    Fix Render deployment

--- Desktop/Project_computer_club_KNRTU/app.js
+++ Desktop/Project_computer_club_KNRTU/app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bookings', require('./routes/bookings'));
 
 app.get('/', (req, res) => {
-  res.redirect('/computer_club.html');
+  res.sendFile(path.join(__dirname, 'public', 'computer_club.html'));
 });
 
 const PORT = process.env.PORT || 3000;
